b7566d93c54388751b0850b60b1ddeaaafb2643f
aabbaq <43639882+aabbaq@users.noreply.github.com>
2022-04-29 10:46:47 +1000

    3A
    Can not pass the speed test: TestSpeed3A
    
    TestPersistPartitionUnreliable3A
    TestPersistPartitionUnreliableLinearizable3A
    
    may fail due to 30s timeout

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,19 @@
 package kvraft
 
-import "6.824/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+	"time"
 
+	"6.824/labrpc"
+)
 
 type Clerk struct {
-	servers []*labrpc.ClientEnd
+	Servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leaderId      int
+	clientId      int64
+	lastRequestId int
 }
 
 func nrand() int64 {
@@ -17,9 +23,12 @@ func nrand() int64 {
 	return x
 }
 
+// set random cilentId
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
-	ck.servers = servers
+	ck.Servers = servers
+	ck.clientId = nrand()
+	// ck.
 	// You'll have to add code here.
 	return ck
 }
@@ -37,9 +46,39 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+	// requestId ready to update
+	updatedRequestId := ck.lastRequestId + 1
+
+	args := GetArgs{
+		Key:       key,
+		ClientId:  ck.clientId,
+		RequestId: updatedRequestId,
+	}
+	DPrintf("Client[%d], Request[%d] Get, Key=%s ", ck.clientId, updatedRequestId, key)
+	for {
+		savedLeaderId := ck.leaderId
+		// make a new reply in every loop
+		var reply GetReply
+
+		// RPC Call
+		if ck.Servers[savedLeaderId].Call("KVServer.Get", &args, &reply) {
+			// Success or No Key -> update request id, return GET value
+			if reply.Err == OK {
+				DPrintf("Get Success: %s", reply.Value)
+				ck.lastRequestId = updatedRequestId
+				return reply.Value
+			} else if reply.Err == ErrNoKey {
+				DPrintf("Get Success, but key not found")
+				ck.lastRequestId = updatedRequestId
+				return ""
+			}
+		}
+		// Fail -> leaderId + 1 and retry
+		DPrintf("Wrong leader[%d], try another one", savedLeaderId)
+		ck.leaderId = (savedLeaderId + 1) % len(ck.Servers)
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 //
@@ -54,6 +93,36 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	// requestId ready to update
+	updatedRequestId := ck.lastRequestId + 1
+	args := PutAppendArgs{
+		Key:       key,
+		Value:     value,
+		Op:        op,
+		ClientId:  ck.clientId,
+		RequestId: updatedRequestId,
+	}
+	DPrintf("Client[%d], Request[%d] PutAppend, Key=%s Value=%s", ck.clientId, updatedRequestId, key, value)
+	for {
+		savedLeaderId := ck.leaderId
+		// make a new reply in every loop
+		var reply PutAppendReply
+
+		// RPC Call
+		if ck.Servers[savedLeaderId].Call("KVServer.PutAppend", &args, &reply) {
+			// Success -> update requestId, no value return
+			if reply.Err == OK {
+				DPrintf("%v Success", op)
+				ck.lastRequestId = updatedRequestId
+				return
+			}
+		}
+
+		// Fail -> leaderId + 1 and retry
+		DPrintf("Wrong leader[%d], try another one", savedLeaderId)
+		ck.leaderId = (savedLeaderId + 1) % len(ck.Servers)
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
